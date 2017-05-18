f338e48df1b9da6065bda2f5512b5bb387602c29
lorenzotara <lorenzo.tarantino@mail.polimi.it>
2017-05-18 12:59:43 +0200

    Finished Player Class Fixed packages

--- src/main/java/it/polimi/ingsw/GC_29/Components/ExcommunicationTile.java
+++ src/main/java/it/polimi/ingsw/GC_29/Components/ExcommunicationTile.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.GC_29.Components;
 
+import it.polimi.ingsw.GC_29.Controllers.BonusAndMalusOnAction;
+import it.polimi.ingsw.GC_29.Controllers.BonusAndMalusOnEffect;
+
 /**
  * Created by Lorenzotara on 17/05/17.
  */
--- src/main/java/it/polimi/ingsw/GC_29/Player/Player.java
+++ src/main/java/it/polimi/ingsw/GC_29/Player/Player.java
@@ -1,8 +1,58 @@
 package it.polimi.ingsw.GC_29.Player;
 
+import it.polimi.ingsw.GC_29.Components.*;
+
 /**
  * Created by Lorenzotara on 18/05/17.
  */
 public class Player {
     private String playerID;
+    private PlayerColor playerColor;
+    private it.polimi.ingsw.GC_29.Components.GameBoard gameboard;
+    private it.polimi.ingsw.GC_29.Components.PersonalBoard personalBoard;
+    private PlayerStatus status;
+    private it.polimi.ingsw.GC_29.Components.LeaderCard[] leaderCards;
+    private it.polimi.ingsw.GC_29.Components.FamilyPawn[] relativePawns;
+    private it.polimi.ingsw.GC_29.Components.Pawn[] excommunicationPawns;
+    private it.polimi.ingsw.GC_29.Components.Pawn[] markerDiscs;
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public PlayerColor getPlayerColor() {
+        return playerColor;
+    }
+
+    public GameBoard getGameboard() {
+        return gameboard;
+    }
+
+    public PersonalBoard getPersonalBoard() {
+        return personalBoard;
+    }
+
+    public PlayerStatus getStatus() {
+        return status;
+    }
+
+    public LeaderCard[] getLeaderCards() {
+        return leaderCards;
+    }
+
+    public FamilyPawn[] getRelativePawns() {
+        return relativePawns;
+    }
+
+    public Pawn[] getExcommunicationPawns() {
+        return excommunicationPawns;
+    }
+
+    public Pawn[] getMarkerDiscs() {
+        return markerDiscs;
+    }
+
+    public void removeLeaderCard(LeaderCard leaderCard) {
+        //TODO: scelta la carta leader da rimuovere, questo metodo la rimuove
+    }
 }
