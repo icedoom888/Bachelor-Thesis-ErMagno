db521b53b23bbd299882b6ffdc939b44cc582483
Christian Sciuto <sciutochristian@gmail.com>
2017-05-18 10:09:10 +0200

    toString() method overrided in some classes. It useful to print the info about that specific type. Methods implemented in PawnSlot, FaithPointsSlot, FaithPointsTrack.

--- src/main/java/it/polimi/ingsw/GC_29/Components/FaithPointsTrack.java
+++ src/main/java/it/polimi/ingsw/GC_29/Components/FaithPointsTrack.java
@@ -5,11 +5,27 @@ package it.polimi.ingsw.GC_29.Components;
  */
 public class FaithPointsTrack implements Cleanable {
     private FaithPointsPawnSlot[] faithPointsTrack;
+    private int numberOfFaithPoints;
+
+    public FaithPointsTrack(int numberOfFaithPoints) {
+        this.faithPointsTrack = new FaithPointsPawnSlot[numberOfFaithPoints];
+    }
 
     public FaithPointsPawnSlot[] getFaithPointsTrack() {
         return faithPointsTrack;
     }
 
     public void movePawn(int position, Pawn pawn) {
+
+        //TODO I'm not sure how to handle this behavior, discuss with Lorenzo and Alberto
+    }
+
+    @Override
+    public void clean() {
+        for (FaithPointsPawnSlot faithPointsPawnSlot : faithPointsTrack) {
+            if(!(faithPointsPawnSlot.isFree())){
+                faithPointsPawnSlot.clearSlot();
+            }
+        }
     }
 }
