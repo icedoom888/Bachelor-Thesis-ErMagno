07b65a5d7aba06f3313a579b6a32cb2807934e0d
lorenzotara <lorenzo.tarantino@mail.polimi.it>
2017-05-18 13:21:46 +0200

    Added Leader Classes

--- src/main/java/it/polimi/ingsw/GC_29/Components/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/GC_29/Components/LeaderCard.java
@@ -1,7 +1,35 @@
 package it.polimi.ingsw.GC_29.Components;
 
+import it.polimi.ingsw.GC_29.Player.Player;
+
 /**
  * Created by Lorenzotara on 18/05/17.
  */
 public class LeaderCard {
+    private String leader;
+    private Requirement[] requirements;
+    private boolean repeatable;
+    private Effect effect;
+
+    public void checkRequirements(Player player) {
+    }
+
+    public void handleEffect() {
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public Requirement[] getRequirements() {
+        return requirements;
+    }
+
+    private boolean isRepeatable() {
+        return repeatable;
+    }
+
+    public Effect getEffect() {
+        return effect;
+    }
 }
---
+++ src/main/java/it/polimi/ingsw/GC_29/Components/Requirement.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.GC_29.Components;
+
+import it.polimi.ingsw.GC_29.Player.Player;
+
+/**
+ * Created by Lorenzotara on 18/05/17.
+ */
+public abstract class Requirement {
+    public void check(Player player) {
+    }
+}
