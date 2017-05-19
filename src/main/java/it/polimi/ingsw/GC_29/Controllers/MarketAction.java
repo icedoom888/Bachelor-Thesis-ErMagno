92cac3fbd6171b5b5e02c29cc17597dd5bbfc992
Christian Sciuto <sciutochristian@gmail.com>
2017-05-19 15:48:47 +0200

    ActionBuilder created, ActionBuilder implemented; TowerActionBuilder structure created.

--- src/main/java/it/polimi/ingsw/GC_29/Controllers/MarketAction.java
+++ src/main/java/it/polimi/ingsw/GC_29/Controllers/MarketAction.java
@@ -1,13 +1,26 @@
 package it.polimi.ingsw.GC_29.Controllers;
 
+import it.polimi.ingsw.GC_29.Components.ActionType;
+import it.polimi.ingsw.GC_29.Components.FamilyPawn;
 import it.polimi.ingsw.GC_29.Player.PlayerStatus;
 
 /**
  * Created by Lorenzotara on 19/05/17.
  */
 public class MarketAction extends Action {
+
+
+    public MarketAction(FamilyPawn pawnSelected, ActionType actionSelected, int workersSelected, boolean realAction, PlayerStatus playerStatus) {
+        super(pawnSelected, actionSelected, workersSelected, realAction, playerStatus);
+    }
+
     @Override
     public void execute(PlayerStatus playerStatus) {
+
+    }
+
+    @Override
+    protected void update(PlayerStatus playerStatus) {
 
     }
 }
