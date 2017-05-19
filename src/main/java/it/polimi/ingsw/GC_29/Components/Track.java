22b367ada31ccfdd1127b139bf0ab4c32a01f9f5
lorenzotara <lorenzo.tarantino@mail.polimi.it>
2017-05-19 10:13:00 +0200

    movePawn and clear of Track implemented

--- src/main/java/it/polimi/ingsw/GC_29/Components/Track.java
+++ src/main/java/it/polimi/ingsw/GC_29/Components/Track.java
@@ -27,19 +27,37 @@ public class Track implements Cleanable {
         return pawnMap.get(pawn);
     }
 
+    /*
+    * movePawn search the position of the pawn in the HashMap,
+    * then search the pawn in the right PawnSlot.
+    * If the Pawn is really there, it changes its position from the index
+    * provided by pawnMap to the new given position, if not it prints
+    * that there has been an error
+     */
     public void movePawn(int position, Pawn pawn) {
+        // TODO: testing
         int index = findPawn(pawn);
         PawnSlot pawnSlot = track[index];
         Pawn realPawn = pawnSlot.findPawn(pawn);
-        track[index].removePawn(realPawn);
-        track[position].addPawn(realPawn);
+
+        if (realPawn != null) {
+            track[index].removePawn(realPawn);
+            track[position].addPawn(realPawn);
+            pawnMap.put(realPawn,position);
+
+        } else {
+            System.out.println("Error: Wrong Pawn");
+        }
     }
 
     @Override
     public void clean() {
         for (PawnSlot pawnSlot : track) {
             pawnSlot.clearSlot();
+            pawnMap.clear();
         }
+
+
     }
 
     public PawnSlot[] getTrack() {
