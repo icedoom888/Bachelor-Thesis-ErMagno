378646a25b70838a7652e2e865eaaaaaa88422a1
AlbertoPennino <alby.penny@gmail.com>
2017-05-20 17:42:28 +0200

    Eliminazione metodo update da ObtainEffect

--- src/main/java/it/polimi/ingsw/GC_29/Components/GoodSetRequirement.java
+++ src/main/java/it/polimi/ingsw/GC_29/Components/GoodSetRequirement.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.GC_29.Components;
 
 import it.polimi.ingsw.GC_29.Player.Player;
+import it.polimi.ingsw.GC_29.Player.PlayerStatus;
 
 /**
  * Created by Lorenzotara on 18/05/17.
@@ -9,7 +10,7 @@ public class GoodSetRequirement extends Requirement {
     private GoodSet goodSet;
 
     @Override
-    public void check(Player player) {
-        super.check(player);
+    public void check(PlayerStatus status) {
+        super.check(status);
     }
 }
