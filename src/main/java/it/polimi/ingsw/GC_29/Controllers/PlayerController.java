e4f3af9d31e328275e49e64b46044b44cc276a6a
Christian Sciuto <sciutochristian@gmail.com>
2017-06-07 19:13:15 +0200

    bug fixed playerController

--- src/main/java/it/polimi/ingsw/GC_29/Controllers/PlayerController.java
+++ src/main/java/it/polimi/ingsw/GC_29/Controllers/PlayerController.java
@@ -70,11 +70,11 @@ public class PlayerController {
      */
     public boolean isPlaceFamilyMemberAction() {
 
-        Boolean answer = player.getAdapter().doAction();
+        //Boolean answer = player.getAdapter().doAction();
 
-        // commute = !commute; //commute = commute==false
+         commute = !commute; //commute = commute==false
 
-        return answer;
+        return commute;
     }
 
 
@@ -82,7 +82,7 @@ public class PlayerController {
 
         // TODO: metodo di interfaccia, qui vi è distribuzione --> chiede quale azione vuole tra le disponibili o se il player vuole vedere le azioni possibili rispetto ad un altra pedina.
 
-        player.getAdapter().showValidActions();
+       // player.getAdapter().showValidActions();
 
 
         System.out.println("");
