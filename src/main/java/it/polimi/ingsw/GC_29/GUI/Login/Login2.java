c372ab53523fcac7fa5ee058d6c9425d40c36bb6
lorenzotara <lorenzo.tarantino@mail.polimi.it>
2017-06-21 21:00:26 +0200

    package login

--- src/main/java/it/polimi/ingsw/GC_29/GUI/Login/Login2.java
+++ src/main/java/it/polimi/ingsw/GC_29/GUI/Login/Login2.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw.GC_29.GUI;
+package it.polimi.ingsw.GC_29.GUI.Login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,13 +10,15 @@ import javafx.stage.Stage;
  * Created by AlbertoPennino on 20/06/2017.
  */
 public class Login2 extends Application {
-    public static void main(String[]args){
-        launch(args);
+
+    public static void main(String[]args) {
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Login2.fxml"));
         stage.setTitle("Login");
         stage.setScene(new Scene(root));
         stage.setMinHeight(310);
