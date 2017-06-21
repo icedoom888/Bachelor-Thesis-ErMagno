0cea578bb6a6b7cf3831b8c195824819882cc62e
lorenzotara <lorenzo.tarantino@mail.polimi.it>
2017-06-21 23:39:58 +0200

    bonus tile

---
+++ src/main/java/it/polimi/ingsw/GC_29/GUI/BonusTile/BonusTileController.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.GC_29.GUI.BonusTile;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by AlbertoPennino on 21/06/2017.
+ */
+public class BonusTileController {
+
+    @FXML private Button button1;
+    @FXML private Button button2;
+    @FXML private Button button3;
+    @FXML private Button button4;
+    @FXML private Button button5;
+
+    public void execute(ActionEvent event) {
+
+    }
+}
