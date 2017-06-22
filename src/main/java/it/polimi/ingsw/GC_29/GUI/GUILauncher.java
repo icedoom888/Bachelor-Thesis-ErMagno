f3276a177a9327a7ee0be2698c73963d2c49d7e5
lorenzotara <lorenzo.tarantino@mail.polimi.it>
2017-06-22 22:55:55 +0200

    New GUI

---
+++ src/main/java/it/polimi/ingsw/GC_29/GUI/GUILauncher.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.GC_29.GUI;
+
+import it.polimi.ingsw.GC_29.GUI.BonusTile.BonusTileController;
+import it.polimi.ingsw.GC_29.GUI.GameBoard.GameBoardController;
+import it.polimi.ingsw.GC_29.GUI.Login.Login2Controller;
+
+/**
+ * Created by AlbertoPennino on 22/06/2017.
+ */
+/*
+public class GUILauncher {
+    GameBoardController gameBoardController;
+    Login2Controller login2Controller;
+    BonusTileController bonusTileController;
+
+    public GUILauncher(){
+       GameBoardController gameBoardController = new GameBoardController();
+       Login2Controller login2Controller = new Login2Controller();
+       BonusTileController bonusTileController = new BonusTileController();
+    }
+
+    public void launchLogin(){
+
+    }
+    public void launchWait(){
+    }
+}
+*/
